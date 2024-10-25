9441e8012290ff061c1e9ece6b028f293cb1b5f8
bgraham2 <bgraham2@gmail.com>
2024-10-25 09:54:33 -0500

    Fixed buttons and add date to text view

--- app/src/main/java/com/example/remindme/MainActivity.java
+++ app/src/main/java/com/example/remindme/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.remindme;
 
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
-
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(new SimpleDateFormat("DD MM-yyyy", Locale.US).format(new Date()));
     }
 }
